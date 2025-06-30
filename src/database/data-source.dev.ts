9259840c7ef82f4a9e2e8fcb5f8991b2a42eaa9d
DuarteDavi <54749367+DuarteDavi@users.noreply.github.com>
2025-06-30 20:16:15 -0300

    username

--- src/database/data-source.dev.ts
+++ src/database/data-source.dev.ts
@@ -4,7 +4,7 @@ export const AppDevDataSource = new DataSource({
   type: 'postgres',
   host: 'localhost',
   port: 5432,
-  username: 'postgres',
+  username: 'chatuser',
   password: process.env.DB_PASSWORD,
   database: 'dev_wschat',
   synchronize: true,
--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -4,7 +4,7 @@ export const AppDataSource = new DataSource({
   type: 'postgres',
   host: 'localhost',
   port: 5432,
-  username: 'postgres',
+  username: 'chatuser',
   password: '12345678',    
   database: 'wschat',
   synchronize: false,
